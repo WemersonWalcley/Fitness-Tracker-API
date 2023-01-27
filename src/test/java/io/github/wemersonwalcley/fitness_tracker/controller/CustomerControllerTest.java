aab65d7fedf036fced1e0288bb8b9ee643bfc39e
WemersonWalcley <wemerson.walcley@outlook.com>
2023-01-27 16:28:47 -0300

    Adding unit tests for all the methods in CustomerController class

---
+++ src/test/java/io/github/wemersonwalcley/fitness_tracker/controller/CustomerControllerTest.java
@@ -0,0 +1,50 @@
+package io.github.wemersonwalcley.fitness_tracker.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import io.github.wemersonwalcley.fitness_tracker.controller.customer.CustomerController;
+import io.github.wemersonwalcley.fitness_tracker.dtos.CustomerDTO;
+import io.github.wemersonwalcley.fitness_tracker.service.customer.CustomerService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+public class CustomerControllerTest {
+
+    @Mock
+    private CustomerService customerService;
+
+    @InjectMocks
+    private CustomerController customerController;
+
+    @Test
+    public void testGetCustomerById() {
+        Long id = 1L;
+        CustomerDTO expectedCustomer = new CustomerDTO();
+
+        when(customerService.getCustomerById(id)).thenReturn(expectedCustomer);
+
+        CustomerDTO actualCustomer = customerController.getCustomerById(id);
+
+        assertEquals(expectedCustomer, actualCustomer);
+        verify(customerService).getCustomerById(id);
+    }
+
+    @Test
+    public void testSave() {
+        CustomerDTO customerDTO = new CustomerDTO();
+        CustomerDTO expectedCustomer = new CustomerDTO();
+
+        when(customerService.save(customerDTO)).thenReturn(expectedCustomer);
+
+        CustomerDTO actualCustomer = customerController.save(customerDTO);
+
+        assertEquals(expectedCustomer, actualCustomer);
+        verify(customerService).save(customerDTO);
+    }
+}
