ce09469cffa7d512afb06a0163dc8d0a9de6b70a
WemersonWalcley <wemerson.walcley@outlook.com>
2023-01-30 17:19:47 -0300

    Cleaning unused methods in AccessLevelEnum class

--- src/main/java/io/github/wemersonwalcley/fitness_tracker/enumeration/AccessLevelEnum.java
+++ src/main/java/io/github/wemersonwalcley/fitness_tracker/enumeration/AccessLevelEnum.java
@@ -1,36 +1,12 @@
 package io.github.wemersonwalcley.fitness_tracker.enumeration;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonFilter;
-import com.fasterxml.jackson.annotation.JsonValue;
-
-import java.util.HashMap;
-import java.util.Map;
-
-@JsonFilter("attributeFilter")
 public enum AccessLevelEnum {
 
     ADMIN("Administrador"),
     USER("Usuario"),
     DEFAULT("Default");
 
-    private String displayName;
-
     AccessLevelEnum(String displayName) {
-        this.displayName = displayName;
-    }
-
-    @JsonCreator
-    public static AccessLevelEnum forValue(String value) {
-        return AccessLevelEnum.valueOf(value);
-    }
-
-    @JsonValue
-    public Map<String, String > jsonValue() {
-        HashMap<String, String> map = new HashMap<>();
-        map.put("name", this.name());
-        map.put("displayName", this.displayName);
-        return map;
     }
 
 }
