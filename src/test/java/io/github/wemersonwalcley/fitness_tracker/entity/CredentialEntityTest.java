9af0a2eebfd8a0e27d30fefb04065adc8c427d8d
WemersonWalcley <wemerson.walcley@outlook.com>
2023-01-29 22:26:35 -0300

    AllArgsConstructor method test in CredentialEntityTest class

--- src/test/java/io/github/wemersonwalcley/fitness_tracker/entity/CredentialEntityTest.java
+++ src/test/java/io/github/wemersonwalcley/fitness_tracker/entity/CredentialEntityTest.java
@@ -1,4 +1,23 @@
 package io.github.wemersonwalcley.fitness_tracker.entity;
 
+import io.github.wemersonwalcley.fitness_tracker.builder.CredentialBuilder;
+import io.github.wemersonwalcley.fitness_tracker.enumeration.AccessLevelEnum;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CredentialEntityTest {
+
+    CredentialEntity credentialBuilder = CredentialBuilder.createObject();
+
+    @Test
+    void testAllArgsConstructor() {
+        CredentialEntity credentialEntity = new CredentialEntity(1L, "Robert Owen", "password", AccessLevelEnum.ADMIN);
+        assertEquals(1L, credentialEntity.getId());
+        assertEquals("Robert Owen", credentialEntity.getUsername());
+        assertEquals("password", credentialEntity.getPassword());
+    }
+
 }
