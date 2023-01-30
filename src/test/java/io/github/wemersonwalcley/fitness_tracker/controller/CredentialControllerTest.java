c45e8ea5cfff0d773565833b2927860007e384a6
WemersonWalcley <wemerson.walcley@outlook.com>
2023-01-30 18:00:51 -0300

    Implementing CredentialController methods unit tests

--- src/test/java/io/github/wemersonwalcley/fitness_tracker/controller/CredentialControllerTest.java
+++ src/test/java/io/github/wemersonwalcley/fitness_tracker/controller/CredentialControllerTest.java
@@ -1,7 +1,43 @@
 package io.github.wemersonwalcley.fitness_tracker.controller;
 
+import io.github.wemersonwalcley.fitness_tracker.controller.credential.CredentialController;
+import io.github.wemersonwalcley.fitness_tracker.dtos.CredentialDTO;
+import io.github.wemersonwalcley.fitness_tracker.dtos.TokenDTO;
+import io.github.wemersonwalcley.fitness_tracker.service.credential.CredentialServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 class CredentialControllerTest {
 
+    @Mock
+    private CredentialServiceImpl loginServiceImpl;
 
+    @InjectMocks
+    private CredentialController controller;
+
+    @Test
+    public void testAuthenticate() {
+        CredentialDTO dto = new CredentialDTO();
+        dto.setUsername("user");
+        dto.setPassword("password");
+        TokenDTO tokenDTO = new TokenDTO();
+        tokenDTO.setLogin("user");
+        tokenDTO.setToken("token");
+        ResponseEntity<TokenDTO> expectedResponse = ResponseEntity.ok(tokenDTO);
+        when(loginServiceImpl.authenticate(any(CredentialDTO.class))).thenReturn(expectedResponse);
+
+        ResponseEntity<TokenDTO> response = controller.authenticate(dto);
+
+        assertEquals(expectedResponse, response);
+    }
 
 }
