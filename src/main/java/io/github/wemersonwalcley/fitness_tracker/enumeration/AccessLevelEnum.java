5f66eb6ca842b6df24e6b71c6838b92aca8db285
WemersonWalcley <wemerson.walcley@outlook.com>
2023-01-31 12:03:23 -0300

    refactor(AccessLevelEnum): Refactoring
    Add get method

--- src/main/java/io/github/wemersonwalcley/fitness_tracker/enumeration/AccessLevelEnum.java
+++ src/main/java/io/github/wemersonwalcley/fitness_tracker/enumeration/AccessLevelEnum.java
@@ -1,12 +1,18 @@
 package io.github.wemersonwalcley.fitness_tracker.enumeration;
 
+import lombok.Getter;
+
+@Getter
 public enum AccessLevelEnum {
 
     ADMIN("Administrador"),
     USER("Usuario"),
     DEFAULT("Default");
 
+    private final String displayName;
+
     AccessLevelEnum(String displayName) {
+        this.displayName = displayName;
     }
 
 }
